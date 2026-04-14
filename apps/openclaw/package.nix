05a46acd5bb1418acdfaccc6fe208f63f6636fbd
Gyutak Kim <gytk.kim@gmail.com>
2026-04-14 23:14:22 +0900

    Make openclaw npm cache writable in nix builds

--- apps/openclaw/package.nix
+++ apps/openclaw/package.nix
@@ -25,6 +25,7 @@ buildNpmPackage {
 
   npmDepsHash = "sha256-c2Ipcza3z24xFHJxUfrJnuybKOglrnLKHQl7lTLoLlQ=";
   sourceRoot = "package";
+  makeCacheWritable = true;
   npmFlags = [ "--legacy-peer-deps" ];
   npmInstallFlags = [ "--legacy-peer-deps" ];
   dontNpmBuild = true;
